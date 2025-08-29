ad1ec663597fcc2e48ccd8cc2043dd7ab0a4b86f
jerinAfrose <jatamanna.s21@gmail.com>
2025-08-29 19:22:17 +0600

    call list and clear btn event listener added

--- index.js
+++ index.js
@@ -18,13 +18,56 @@ copyButtons.forEach((btn) => {
   });
 });
 
-// calling alert in call button
+// calling alert in call button &
+// coin deduct from coin section
 const callBtns = document.querySelectorAll(".call-btn");
+
+const coinCnt = document.getElementById("coin-cnt");
+
+const callList = document.getElementById("call-list");
 callBtns.forEach((btn) => {
   btn.addEventListener("click", function () {
     //alert(`calling omuk`);
-    const service = btn.dataset.service;
-    const number = btn.dataset.number;
-    alert(`calling ${service}:: ${number}...`);
+    const categoryNAME = btn.dataset.service;
+    const phoneNumber = btn.dataset.number;
+    let coin = parseInt(coinCnt.textContent);
+    if (coin < 20) {
+      alert(`You don't have enough coin for calling. Minimum 20 coins needed.`);
+      return;
+    }
+    alert(`calling ${categoryNAME}:: ${phoneNumber}...`);
+
+    coinCnt.textContent = coin - 20;
+    // if (coin < 20) {
+    //   alert(`You don't have enough coin for calling. Minimum 20 coins needed.`);
+    // }
+
+    // call history add
+    document.getElementById("default-list").style.display = "none";
+    const li = document.createElement("li");
+    const realTime = new Date().toLocaleTimeString();
+    li.classList.add(
+      "space-y-3",
+      "text-sm",
+      "text-gray-700",
+      "bg-[#fafafa]",
+      "rounded-lg",
+      "p-4",
+      "mt-3"
+    );
+    li.innerHTML = `
+    <div class="flex justify-between">
+    <div>
+    <strong>${categoryNAME}</strong> <br> 
+    <p class="text-gray-500 text-xs">${phoneNumber}</p></div>
+    <span class="text-sm">${realTime}</span></div>
+    `;
+    callList.appendChild(li);
   });
+});
+
+// call list clear event listener
+const clearCallList = document.getElementById("clear-call-list");
+clearCallList.addEventListener("click", function () {
+  callList.innerHTML = "";
 });
