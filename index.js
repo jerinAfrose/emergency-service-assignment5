4617794339a0a4de1760f2e6d2af62d19dcb60f0
jerinAfrose <jatamanna.s21@gmail.com>
2025-08-29 18:01:43 +0600

    call button alert added

--- index.js
+++ index.js
@@ -7,3 +7,24 @@ heartIcons.forEach((icon) => {
     redHeartCount.textContent = count + 1;
   });
 });
+
+// copy button count event handler
+const copyCount = document.getElementById("copy-count-btn");
+const copyButtons = document.querySelectorAll(".copy-btn");
+copyButtons.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    let current = parseInt(copyCount.textContent);
+    copyCount.textContent = current + 1;
+  });
+});
+
+// calling alert in call button
+const callBtns = document.querySelectorAll(".call-btn");
+callBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    //alert(`calling omuk`);
+    const service = btn.dataset.service;
+    const number = btn.dataset.number;
+    alert(`calling ${service}:: ${number}...`);
+  });
+});
